001ba236b5972c8250ba531638da825aa388632a
Shaun-Mifsud <32672907+Shaun-Mifsud@users.noreply.github.com>
2021-12-02 14:23:48 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  ngOnInit () {
+    const container = document.getElementById("sheetMusic");
+    const osmd = new OpenSheetMusicDisplay(container, {
+      autoResize: true, // just an example for an option, no option is necessary.
+      backend: "svg",
+      drawTitle: true,
+      // put further options here
+    });
+    
+}
 }
